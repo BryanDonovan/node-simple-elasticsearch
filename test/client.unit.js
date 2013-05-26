968053408a1dc5169df7568116875a8db3550ec8
Bryan Donovan <bdondo@gmail.com>
2013-05-26 13:49:00 -0700

    adding refresh() and allowing multiple indices to be passed into status()

--- test/client.unit.js
+++ test/client.unit.js
@@ -149,6 +149,39 @@ describe("client.js", function () {
                     });
                 });
 
+                context("when array of indices passed in", function () {
+                    var new_index_name1;
+                    var new_index_name2;
+
+                    beforeEach(function (done) {
+                        new_index_name1 = index_name + support.random.string();
+                        new_index_name2 = index_name + support.random.string();
+
+                        client.indices.create({index: new_index_name1, options: {number_of_shards: 1}}, function (err) {
+                            check_err(err);
+                            client.indices.create({index: new_index_name2, options: {number_of_shards: 1}}, done);
+                        });
+                    });
+
+                    afterEach(function (done) {
+                        client.indices.del({index: new_index_name1}, function (err) {
+                            check_err(err);
+                            client.indices.del({index: new_index_name2}, done);
+                        });
+                    });
+
+                    it("returns status for all supplied indices", function (done) {
+                        client.indices.status({indices: [new_index_name1, new_index_name2]}, function (err, result) {
+                            check_err(err);
+                            assert.strictEqual(result.ok, true);
+                            var expected_indices = [new_index_name1, new_index_name2].sort;
+                            var actual_indices = Object.keys(result.indices).sort;
+                            assert.deepEqual(actual_indices, expected_indices);
+                            done();
+                        });
+                    });
+                });
+
                 context("when no args passed in", function () {
                     it("returns all index statuses", function (done) {
                         client.indices.status(function (err, result) {
@@ -171,7 +204,7 @@ describe("client.js", function () {
             });
 
             describe("del()", function () {
-                context("when no args passed in", function () {
+                context("when null args passed in", function () {
                     it("returns an error", function (done) {
                         client.indices.del(null, function (err) {
                             assert.ok(err.message.match(/args required/));
@@ -200,7 +233,7 @@ describe("client.js", function () {
                     });
                 });
 
-                context("when index does exist", function () {
+                context("when index exists", function () {
                     var new_index_name;
 
                     beforeEach(function (done) {
@@ -229,6 +262,135 @@ describe("client.js", function () {
                                 assert.ok(result.error.match(/IndexMissingException/));
                                 assert.strictEqual(result.status, 404);
                                 done();
+                            });
+                        });
+                    });
+                });
+            });
+
+            describe("refresh()", function () {
+                context("when index passed in", function () {
+                    context("and index does not exist", function () {
+                        it("returns result with IndexMissingException and status 404", function (done) {
+                            var non_existent_index = index_name + support.random.string(20);
+                            client.indices.refresh({index: non_existent_index}, function (err, result) {
+                                assert.ok(result.error.match(/IndexMissingException/));
+                                assert.strictEqual(result.status, 404);
+                                done();
+                            });
+                        });
+                    });
+
+                    context("and index exists", function () {
+                        var new_index_name;
+
+                        beforeEach(function (done) {
+                            new_index_name = index_name + support.random.string();
+                            client.indices.create({index: new_index_name, options: {number_of_shards: 1}}, done);
+                        });
+
+                        afterEach(function (done) {
+                            client.indices.del({index: new_index_name}, done);
+                        });
+
+                        it("refreshes the index", function (done) {
+                            client.indices.status({index: new_index_name}, function (err, result) {
+                                check_err(err);
+                                var initial_request_count = result.indices[new_index_name].refresh.total;
+                                assert.ok(initial_request_count);
+
+                                client.indices.refresh({index: new_index_name}, function (err, result) {
+                                    check_err(err);
+                                    assert.strictEqual(result.ok, true);
+
+                                    client.indices.status({index: new_index_name}, function (err, result) {
+                                        check_err(err);
+                                        var final_request_count = result.indices[new_index_name].refresh.total;
+                                        assert.strictEqual(final_request_count, initial_request_count + 1);
+                                        done();
+                                    });
+                                });
+                            });
+                        });
+                    });
+                });
+
+                context("when array of indexes passed in", function () {
+                    var new_index_name1;
+                    var new_index_name2;
+
+                    beforeEach(function (done) {
+                        new_index_name1 = index_name + support.random.string();
+                        new_index_name2 = index_name + support.random.string();
+
+                        client.indices.create({index: new_index_name1, options: {number_of_shards: 1}}, function (err) {
+                            check_err(err);
+                            client.indices.create({index: new_index_name2, options: {number_of_shards: 1}}, done);
+                        });
+                    });
+
+                    afterEach(function (done) {
+                        client.indices.del({index: new_index_name1}, function (err) {
+                            check_err(err);
+                            client.indices.del({index: new_index_name2}, done);
+                        });
+                    });
+
+                    it("refreshes the indices", function (done) {
+                        client.indices.status({indices: [new_index_name1, new_index_name2]}, function (err, result) {
+                            check_err(err);
+                            var initial_request_count1 = result.indices[new_index_name1].refresh.total;
+                            var initial_request_count2 = result.indices[new_index_name2].refresh.total;
+                            assert.ok(initial_request_count1);
+                            assert.ok(initial_request_count2);
+
+                            client.indices.refresh({indices: [new_index_name1, new_index_name2]}, function (err, result) {
+                                check_err(err);
+                                assert.strictEqual(result.ok, true);
+
+                                client.indices.status({indices: [new_index_name1, new_index_name2]}, function (err, result) {
+                                    check_err(err);
+                                    var final_request_count1 = result.indices[new_index_name1].refresh.total;
+                                    var final_request_count2 = result.indices[new_index_name2].refresh.total;
+
+                                    assert.strictEqual(final_request_count1, initial_request_count1 + 1);
+                                    assert.strictEqual(final_request_count2, initial_request_count2 + 1);
+
+                                    done();
+                                });
+                            });
+                        });
+                    });
+                });
+
+                context("when no args passed in", function () {
+                    function get_refresh_total(indices) {
+                        var total = 0;
+
+                        Object.keys(indices).forEach(function (key) {
+                            total += indices[key].refresh.total;
+                        });
+
+                        return total;
+                    }
+
+                    it("refreshes all indexes", function (done) {
+                        client.indices.status(function (err, result) {
+                            check_err(err);
+                            var initial_request_count = get_refresh_total(result.indices);
+                            assert.ok(initial_request_count);
+
+                            client.indices.refresh(function (err, result) {
+                                check_err(err);
+                                assert.strictEqual(result.ok, true);
+
+                                client.indices.status(function (err, result) {
+                                    check_err(err);
+                                    var final_request_count = get_refresh_total(result.indices);
+                                    var min_diff = Object.keys(result.indices).length; // at least one refresh per index.
+                                    assert.ok(final_request_count >= initial_request_count + min_diff);
+                                    done();
+                                });
                             });
                         });
                     });
